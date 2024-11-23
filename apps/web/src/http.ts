52c9b86f85b79f84d204215ccd473f3ca954bbc6
Paul Yang <sea5122916@gmail.com>
2024-11-24 03:14:00 +0900

    feat: remote 페이지 작성중

--- apps/web/src/http.ts
+++ apps/web/src/http.ts
@@ -2,10 +2,14 @@ import { createHttp, createInstance } from "@internal/http";
 
 const httpInstance = createInstance({
   headers: {
-    Authorization: `Bearer`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJpYXQiOjE3MzUwODQ4MDB9.cmLnN3Qu2GmO0Bvh1Xbc_7rvL5v1L6f9A38ybrW84BA`,
   },
+  'credentials':"include",
+  'prefixUrl':'http://skyofseoul.synology.me:44333'
 });
 
 export const authHttp = createHttp(httpInstance);
 
-export const http = createHttp();
+export const http = createHttp({
+  'prefixUrl':'http://skyofseoul.synology.me:44333'
+});
