1d5ad68105dd8034fb0bed897f67a228951269ce
devgiljong <calf-love@naver.com>
2024-11-24 04:04:55 +0900

    feat: 로그인 연동

--- apps/web/app/sign-up/page.tsx
+++ apps/web/app/sign-up/page.tsx
@@ -5,27 +5,27 @@ import { useFunnel } from "~/src/shared/hooks/use-funnel";
 import { IdAndPasswordStep } from "./_components/IdAndPasswordStep";
 import { NickNameStep } from "./_components/NicknameAndColorStep";
 import { Stack } from "~/src/shared/ui/Stack";
+import { useRouter } from "next/navigation";
+import { $Routes } from "~/src/routes";
 
 export default function Page() {
   const [Funnel, onNext] = useFunnel(["password", "nickname"] as const);
-
-  const [state, setState] = useState({
-    id: "",
-    password: "",
-    nickname: "",
-    color: "",
-  });
+  const router = useRouter();
 
   return (
     <Funnel>
       <Funnel.Step name={"password"}>
         <PaddingLayout>
-          <IdAndPasswordStep onNext={() => onNext("nickname")} id={state.id} password={state.password} />
+          <IdAndPasswordStep onNext={() => onNext("nickname")} id={""} password={""} />
         </PaddingLayout>
       </Funnel.Step>
       <Funnel.Step name={"nickname"}>
         <PaddingLayout>
-          <NickNameStep onNext={() => {}} />
+          <NickNameStep
+            onNext={() => {
+              router.push($Routes.calendar());
+            }}
+          />
         </PaddingLayout>
       </Funnel.Step>
     </Funnel>
