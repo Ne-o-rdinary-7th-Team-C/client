4a8e55e516db0d23b3cc9f0878cb8b9636a0842e
Paul Yang <sea5122916@gmail.com>
2024-11-24 07:33:54 +0900

    feat: 질문 보내기 done

---
+++ apps/web/app/donequestions/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { QuestionSuccess } from "~/src/features/question-answer/QuestionSuccess";
+
+export default function QuestionSuccessPage() {
+  return (
+    <div>
+      <QuestionSuccess questionAuthorName={"핸"} />
+    </div>
+  );
+}
--- apps/web/src/features/question-answer/QuestionSuccess.tsx
+++ apps/web/src/features/question-answer/QuestionSuccess.tsx
@@ -1,12 +1,15 @@
 import { Text } from "~/src/shared/ui/text";
+import { useRouter } from "next/navigation";
 import { Button } from "~/src/shared/ui/button";
 import Image from "next/image";
+import { toast } from "sonner";
 
 type Props = {
   questionAuthorName: string;
 };
 
 export const QuestionSuccess = (props: Props) => {
+  const router = useRouter();
   return (
     <div className={"h-[calc(100vh-80px)] py-[40px] px-[16px] text-center flex flex-col justify-center"}>
       <Text variant="display02">질문이 작성됐어요!</Text>
@@ -18,8 +21,10 @@ export const QuestionSuccess = (props: Props) => {
         height={160}
       />
       <div className="flex flex-col gap-[12px] mt-auto">
-        <Button size="lg">나만의 어드벤트 캘린더 만들기</Button>
-        <Button size="lg" variant="outline">
+        <Button size="lg" onClick={() => toast.error("기능준비중입니다!")}>
+          나만의 어드벤트 캘린더 만들기
+        </Button>
+        <Button size="lg" variant="outline" onClick={() => router.push("/calendar-view/32")}>
           {props.questionAuthorName}님의 답변 보러가기
         </Button>
       </div>
