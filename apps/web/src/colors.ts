485bc7d2dbf9dcae0626c426f8bc7b45fb20af35
hyesngy <tell280210@gmail.com>
2024-11-23 16:47:52 +0900

    feat: colors 추가

---
+++ apps/web/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  black: "#171717",
+  highEmphasis: "#2F3033",
+  subText: "#5E6166",
+  midEmphasis: "#8D9199",
+  lowEmphasis: "#C0C4CC",
+  disabled: "#DFE1E5",
+  line: "#EBEDF1",
+  white: "#FFFFFF",
+
+  primaryColor1: "#DC2244",
+  primaryColor2: "#F2274C",
+  primaryColor3: "#FBBEC9",
+
+  illustColor1: "#F2274C",
+  illustColor2: "#64BF39",
+  illustColor3: "#FECD57",
+} as const;
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,16 +1,17 @@
 import type { Config } from "tailwindcss";
+import { colors } from "./src/colors";
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        ...colors,
       },
     },
   },
