d91119dd6e3f5a5c2ba02c7b6f2cc2e8dc05b105
devgiljong <calf-love@naver.com>
2024-11-24 04:13:08 +0900

    feat: 드로어 기능 구현

--- apps/web/app/calendar/page.tsx
+++ apps/web/app/calendar/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { socialDrawer } from "~/src/features/social-share/Drawer";
+import { Button } from "~/src/shared/ui/button";
+import { FixedBottom } from "~/src/shared/ui/FixedBottom";
+import { Stack } from "~/src/shared/ui/Stack";
+import { Text } from "~/src/shared/ui/text";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <Stack className=" px-[26px] py-6 bg-[#DC2244] h-screen">
+      <Text className=" text-gray-50  whitespace-pre-wrap">{`aiaiaiaiai's \n Advent calendar`}</Text>
+      <FixedBottom className=" pb-[48px] px-[16px]">
+        <Button
+          size={"lg"}
+          variant={"primary"}
+          onClick={() => {
+            socialDrawer.open({ link: "/dsa" });
+          }}
+        >
+          내 캘린더 공유하기
+        </Button>
+      </FixedBottom>
+    </Stack>
+  );
 }
