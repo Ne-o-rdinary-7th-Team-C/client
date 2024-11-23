5d61fa37bd31ff13684e68757ba35dbc86af8154
devgiljong <calf-love@naver.com>
2024-11-24 05:17:14 +0900

    feat: page add

--- apps/web/app/calendar/page.tsx
+++ apps/web/app/calendar/page.tsx
@@ -9,18 +9,31 @@ import { Button } from "~/src/shared/ui/button";
 import { FixedBottom } from "~/src/shared/ui/FixedBottom";
 import { Stack } from "~/src/shared/ui/Stack";
 import { Text } from "~/src/shared/ui/text";
+import { SuspenseQuery } from "@suspensive/react-query";
+import { ErrorBoundary, Suspense } from "@suspensive/react";
 
 export default function Page() {
-  const { data: calendars } = useQuery(userQueryQuestions());
-
   return (
     <Stack className=" px-[26px] py-6 bg-[#DC2244] h-screen">
-      <Text className=" text-gray-50  whitespace-pre-wrap text-[32px] font-bold">{`aiaiaiaiai's \nAdvent calendar`}</Text>
-      <Stack className=" grid grid-cols-3 gap-20">
-        {calendars?.success.map((calendar) => (
-          <AdventCalendarButton key={calendar.question_id}>{calendar.assigned_date}</AdventCalendarButton>
-        ))}
-      </Stack>
+      <ErrorBoundary fallback={null}>
+        <Suspense>
+          <Text className=" text-gray-50  whitespace-pre-wrap text-[32px] font-bold">{`CMC Team C's \nAdvent calendar`}</Text>
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary fallback={<ErrorFallback />}>
+        <Suspense>
+          <Stack className=" grid grid-cols-3 gap-20">
+            <SuspenseQuery {...userQueryQuestions()}>
+              {({ data: calendars }) =>
+                calendars?.success.map((calendar) => (
+                  <AdventCalendarButton key={calendar.question_id}>{calendar.assigned_date}</AdventCalendarButton>
+                ))
+              }
+            </SuspenseQuery>
+          </Stack>
+        </Suspense>
+      </ErrorBoundary>
       <FixedBottom className=" pb-[48px] px-[16px]">
         <Button
           size={"lg"}
@@ -35,3 +48,18 @@ export default function Page() {
     </Stack>
   );
 }
+
+const ErrorFallback = () => {
+  return (
+    <Stack className="  h-[50vh]">
+      <Stack className=" gap-y-2 items-center">
+        <Text variant={"body-long-02"} className=" text-gray-50 ">
+          죄송해요
+        </Text>
+        <Text variant={"body-long-02"} className=" text-gray-50">
+          오류가 발생했어요
+        </Text>
+      </Stack>
+    </Stack>
+  );
+};
