d3e6218f79a2d991efaefa7ef07000bd24541f4e
devgiljong <calf-love@naver.com>
2024-11-23 20:46:06 +0900

    feat: add dialog and precommit process add

--- apps/web/src/shared/ui/FixedBottom.tsx
+++ apps/web/src/shared/ui/FixedBottom.tsx
@@ -1,3 +1,6 @@
-export const FixedBottom = () => {
-  return <div></div>;
+import { ComponentPropsWithoutRef } from "react";
+
+export const FixedBottom = (props: ComponentPropsWithoutRef<"div">) => {
+  const { className, children, ...rest } = props;
+  return <div className=" fixed bottom-0 left-[50%] translate-x-[-50%] w-screen max-w-[450px]"></div>;
 };
